b6be1f22c0df87fcd72c3c8de255daaecb59a155
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-06-24 11:36:58 +0200

    Updates

--- main.js
+++ main.js
@@ -5,4 +5,13 @@ const shareButton = document.getElementById('share-button');
 shareButton.addEventListener('click', () => {
   const isActive = cardFooter.classList.toggle('active');
   sharePanel.setAttribute('aria-hidden', !isActive);
+  shareLinks.forEach((link) => {
+    if (visible) {
+      link.setAttribute('tabindex', '0');
+      link.setAttribute('href', '#'); // ou l'URL correcte
+    } else {
+      link.setAttribute('tabindex', '-1');
+      link.removeAttribute('href');
+    }
+  });
 });
