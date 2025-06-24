b1edda58fd0e3ceead12be725faa771a0f7b160b
Bastien Ricard <bastien.ricard-dev@outlook.com>
2025-06-24 10:59:52 +0200

    Updates

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-const isProd = location.hostname === 'bricard-dev.github.io';
-const css = document.createElement('link');
 const cardFooter = document.getElementById('card-footer');
 const sharePanel = document.getElementById('share-panel');
 const shareButton = document.getElementById('share-button');
-
-css.rel = 'stylesheet';
-css.href = isProd
-  ? '/fm_article-preview-component/css/main.css'
-  : './css/main.css';
-document.head.appendChild(css);
 
 shareButton.addEventListener('click', () => {
   const isActive = cardFooter.classList.toggle('active');
